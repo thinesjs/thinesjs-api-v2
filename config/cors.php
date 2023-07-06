3fc1a6bb0fd850ef0df616e6e5e3c8d58bc32fa7
thinesjs <thinesjs@gmail.com>
2023-07-06 13:06:58 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['thinesjaishankar.com', 'dev.thinesjaishankar.com', 'www.thinesjaishankar.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
